0b81e11359df0b64329eaf398b4f7b113624a3ac
Amos <amos@cogentmail.com>
2026-02-25 11:02:28 +0530

    Add navigation sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
 const Sidebar = () => {
-    return (
-        <div className="sidebar">
-            {/* Sidebar content goes here */}
-        </div>
-    );
+  return (
+    <div className="sidebar">
+      <h2>Navigation</h2>
+      <ul>
+        <li><a href="/dashboard">Dashboard</a></li>
+        <li><a href="/rules">Rules</a></li>
+        <li><a href="/create">Create Rule</a></li>
+        <li><a href="/edit">Edit Rule</a></li>
+        <li><a href="/report">Generate Report</a></li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
